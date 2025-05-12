8d865437302f40f783e77db8bc8929749477382c
kurobast <loregimenez2013.lu@gmail.com>
2025-05-12 13:18:58 +0200

    corrección gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/Kurobast/react-rick-and-morty",
+  base: "/react-rick-and-morty",
   plugins: [react()],
 })
