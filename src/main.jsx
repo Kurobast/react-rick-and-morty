05784fb360edd10dc3c177d745cd21ed8e07b201
kurobast <loregimenez2013.lu@gmail.com>
2025-05-12 14:01:06 +0200

    correcciones proyecto rick-and-morty

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // Usa HashRouter
+import { HashRouter } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
